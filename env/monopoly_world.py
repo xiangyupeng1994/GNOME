ac017bd76d65d1c8b6610e26316cbbe33a892cda
Becky <xpeng62@gatech.edu>
2020-02-17 19:30:07 -0500

    021720

---
+++ env/monopoly_world.py
@@ -0,0 +1,8 @@
+from monopoly-simulator.gameplay_simple_becky_v1 import *
+
+class Monopoly_simple(object):
+    def __init__(self):
+
+    def next(self,action):
+        before_agent(game_elements, num_active_players, num_die_rolls, current_player_index):
+        return self.current_state, reward, terminal, {}
